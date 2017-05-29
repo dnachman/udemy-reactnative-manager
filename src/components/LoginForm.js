e27d1be8aa2dd7992fb232891c0c49364aa31ba3
David Nachman <david@nachman.org>
2017-05-29 17:04:00 -0400

    finished login with spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Text } from 'react-native';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 
@@ -15,6 +16,17 @@ class LoginForm extends Component {
 		const { email, password } = this.props;
 		this.props.loginUser({ email, password });
 	}
+	renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large" />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
+    }
 	render() {
 		return (
 			<Card>
@@ -35,21 +47,28 @@ class LoginForm extends Component {
 						value={this.props.password}
 					/>
 				</CardSection>
+                <Text style={styles.errorTextStyle}>
+                    {this.props.error}
+                </Text>
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+                    {this.renderButton()}
 				</CardSection>
 			</Card>
 		);
 	}
 }
 
-const mapStateToProps = state => {
-	return {
-		email: state.auth.email,
-		password: state.auth.password
-	};
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
+const mapStateToProps = ({ auth }) => {
+    const { email, password, error, loading } = auth;
+	return { email, password, error, loading };
 };
 
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
